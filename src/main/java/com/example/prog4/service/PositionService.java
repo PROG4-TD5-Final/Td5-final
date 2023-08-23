5f5a7d6560189ba86d47e30ad2cf66dbbe5beff0
fetraandri <hei.fetra@gmail.com>
2023-08-23 00:48:02 -0400

    Fixed Issue and Added Secondary Database cnaps Support

--- src/main/java/com/example/prog4/service/PositionService.java
+++ src/main/java/com/example/prog4/service/PositionService.java
@@ -1,7 +1,7 @@
 package com.example.prog4.service;
 
-import com.example.prog4.repository.PositionRepository;
-import com.example.prog4.repository.entity.Position;
+import com.example.prog4.repository.database1.PositionRepository;
+import com.example.prog4.repository.database1.entity.Position;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
