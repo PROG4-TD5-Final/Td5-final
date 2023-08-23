8389dafe276cce3d0e5ee60f6b3b846a1082579c
fetraandri <hei.fetra@gmail.com>
2023-08-23 16:16:02 -0400

    fix: cnaps and constraint

--- src/main/java/com/example/prog4/repository/Repository.java
+++ src/main/java/com/example/prog4/repository/Repository.java
@@ -2,6 +2,7 @@ package com.example.prog4.repository;
 
 import com.example.prog4.repository.database1.entity.Employee;
 import com.example.prog4.model.EmployeeFilter;
+import com.example.prog4.repository.database2.entity.CnapsEmployee;
 
 import java.util.List;
 
@@ -9,4 +10,5 @@ import java.util.List;
 public interface Repository {
     Employee findById(String idEmployee);
     void save(Employee toSave);
+
 }
